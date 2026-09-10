a4b8ce8496b1a9e4823a351ba68d24ae60371830
Marcelo Pozzi <marcelopozzi25@gmail.com>
2026-09-10 20:59:04 -0300

    Primer backend de DESCUENTApp

---
+++ database.py
@@ -0,0 +1,32 @@
+﻿import sqlite3
+
+conexion = sqlite3.connect("descuentapp.db")
+cursor = conexion.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS ofertas (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    supermercado TEXT NOT NULL,
+    titulo TEXT NOT NULL,
+    banco TEXT,
+    categoria TEXT
+)
+""")
+
+cursor.execute("DELETE FROM ofertas")
+
+ofertas = [
+    ("COTO", "20% de descuento", "BBVA", "Supermercados"),
+    ("Carrefour", "25% de descuento", "Banco Nación", "Supermercados"),
+    ("DIA", "30% de descuento", "BBVA", "Supermercados")
+]
+
+cursor.executemany("""
+INSERT INTO ofertas (supermercado, titulo, banco, categoria)
+VALUES (?, ?, ?, ?)
+""", ofertas)
+
+conexion.commit()
+conexion.close()
+
+print("Base de datos y ofertas cargadas correctamente.")
---
+++ main.py
@@ -0,0 +1,45 @@
+﻿import sqlite3
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def inicio():
+    return {
+        "mensaje": "Bienvenido al backend de DESCUENTApp!",
+        "estado": "funcionando"
+    }
+
+
+@app.get("/ofertas")
+def obtener_ofertas():
+    conexion = sqlite3.connect("descuentapp.db")
+    conexion.row_factory = sqlite3.Row
+
+    cursor = conexion.cursor()
+    cursor.execute("SELECT * FROM ofertas")
+
+    ofertas = [dict(fila) for fila in cursor.fetchall()]
+    conexion.close()
+
+    return ofertas
+
+@app.get("/ofertas/banco/{banco}")
+def obtener_ofertas_por_banco(banco: str):
+    conexion = sqlite3.connect("descuentapp.db")
+    conexion.row_factory = sqlite3.Row
+
+    cursor = conexion.cursor()
+
+    cursor.execute(
+        "SELECT * FROM ofertas WHERE banco = ?",
+        (banco,)
+    )
+
+    ofertas = [dict(fila) for fila in cursor.fetchall()]
+
+
+    conexion.close()
+
+    return ofertas
